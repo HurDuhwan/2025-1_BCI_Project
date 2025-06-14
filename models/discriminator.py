0b753056d2ccf6bbb8bbeff6d8b0237a617eeebe
HurDuhwan <gjenghks@inha.edu>
2025-06-14 14:17:43 +0000

    fix: update DomainDiscriminator architecture to match design

--- models/discriminator.py
+++ models/discriminator.py
@@ -7,9 +7,13 @@ class DomainDiscriminator(nn.Module):
         self.hidden_dim = int(in_dim/2)
         self.net = nn.Sequential(
             nn.Linear(in_dim, self.hidden_dim),
+            nn.ReLU(),
             nn.Linear(self.hidden_dim, self.hidden_dim),
+            nn.ReLU(),
             nn.Linear(self.hidden_dim, self.hidden_dim),
+            nn.ReLU(),
             nn.Linear(self.hidden_dim, 1),
+            # nn.ReLU(),
         )
     def forward(self, x):
         return self.net(x)
